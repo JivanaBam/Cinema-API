11f82b0055186754236fea63231869e19014f9d8
JivanaBam <jivanabam99@gmail.com>
2024-06-18 13:03:02 +0545

    deploy path

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 // Cross origin Resource Sharing
 
 const corsOptions = {
-  origin: ["https://cinema-two-mu.vercel.app/"],
+  origin: ["http://cinema-two-mu.vercel.app/"],
   optionsSuccessStatus: 200,
 };
 
